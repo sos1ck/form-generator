8200512a4e5017a8cb28f9a35fdc695de348e65b
Jakub Juraszek <jakubjuraszek@yahoo.com>
2020-02-29 17:35:56 +0100

    Updates

---
+++ precache-manifest.23ab94dda11595805bf01e92e42cea18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf6cab3da55b9141df078a6205c7ef5",
+    "url": "/form-generator/index.html"
+  },
+  {
+    "revision": "64e965ea73d84d3556d1",
+    "url": "/form-generator/static/css/main.a0f350df.chunk.css"
+  },
+  {
+    "revision": "6e2c31f4bea1e0b02c5a",
+    "url": "/form-generator/static/js/2.30f89c66.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/form-generator/static/js/2.30f89c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64e965ea73d84d3556d1",
+    "url": "/form-generator/static/js/main.43b71ea4.chunk.js"
+  },
+  {
+    "revision": "261f826b49687a4c949c",
+    "url": "/form-generator/static/js/runtime-main.5f072658.js"
+  }
+]);
